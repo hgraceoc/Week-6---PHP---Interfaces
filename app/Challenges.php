2652965c564407e177999275b565284d3dc6cecb
Hannah Cryer <hannahcryer@hotmail.com>
2020-05-08 14:41:35 +0100

    Extrude the shapes

--- app/Challenges.php
+++ app/Challenges.php
@@ -11,9 +11,11 @@ class Challenges
         echo "\nChallenges\n";
 
         $this->shapes();
-        // $this->extrude();
+        $this->extrude();
     
     }
+
+    //Question One
 
     public function shapes()
     {
@@ -31,33 +33,35 @@ class Challenges
             $rectangle->area() // 20
         );
     }
+
+    //Question Two
+
+    public function extrude()
+{
+    echo "\n02) Extrude\n";
+
+    // create 2D objects
+    $square = new Shapes\Square(4);
+    $circle = new Shapes\Circle(4);
+    $rectangle = new Shapes\Rectangle(4, 5);
+
+    // turn into 3D objects
+    $cube = new Shapes\Extrude($square, 4);
+    $cylinder = new Shapes\Extrude($circle, 4);
+    $cuboid = new Shapes\Extrude($rectangle, 7);
+
+    // log the volumes of each
+    dump(
+        $cube->volume(), // 64
+        $cylinder->volume(), // 201.06192982975
+        $cuboid->volume() // 140
+    );
+
+    try {
+        new Shapes\Extrude($cube, 12); // shouldn't work
+        dump("Accepted. Oops!"); // if you see this, something's not right
+    } catch (\TypeError $e) {
+        dump("Not accepted. Good work!"); // if you see this, you've done it right
+    }
 }
-
-//     public function extrude()
-// {
-//     echo "\n02) Extrude\n";
-
-//     // create 2D objects
-//     $square = new Shapes\Square(4);
-//     $circle = new Shapes\Circle(4);
-//     $rectangle = new Shapes\Rectangle(4, 5);
-
-//     // turn into 3D objects
-//     $cube = new Shapes\Extrude($square, 4);
-//     $cylinder = new Shapes\Extrude($circle, 4);
-//     $cuboid = new Shapes\Extrude($rectangle, 7);
-
-//     // log the volumes of each
-//     dump(
-//         $cube->volume(), // 64
-//         $cylinder->volume(), // 201.06192982975
-//         $cuboid->volume() // 140
-//     );
-
-//     try {
-//         new Shapes\Extrude($cube, 12); // shouldn't work
-//         dump("Accepted. Oops!"); // if you see this, something's not right
-//     } catch (\TypeError $e) {
-//         dump("Not accepted. Good work!"); // if you see this, you've done it right
-//     }
-// }
+}
--- app/Shapes/Extrude.php
+++ app/Shapes/Extrude.php
@@ -4,8 +4,25 @@ declare(strict_types=1);
 
 namespace App\Shapes;
 
-class Extrude implements ShapeInterface
+class Extrude 
+//implements ShapeInterface - although this is used in the shape class, it is not used in the extrude class as they are two separate classes and have different rules
 {
+    private $depth;
+    private $shape;
 
+    public function __construct(ShapeInterface $shape, $depth)
+    //using the shape interface class on shape parameter, rather than the whole function itself - so the shape in run through that class
+    {
+        $this->depth = $depth;
+        $this->shape = $shape;    
+    }
+
+    public function volume()
+    {
+        return $this->shape->area() * $this->depth;
+        //calling the area function within the shape class, which we accepted above to times that by depth, which exists in this class but is not influenced by the shape class specifications or functions
+    }
 
 } 
+
+// Create an Extrude class in the App\Shapes namespace. On construction it should take a class that implements ShapeInterface as well as a depth. It should have a volume() method which returns the volume (area × depth) of the given shape. Make sure you use type-hinting to make sure you can't pass non-shapes into Extrude.
